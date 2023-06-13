66c1a7ccfcf6a26803b6e94b6eaa3bab06653c14
sun <743685264@qq.com>
2023-06-14 00:16:09 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,17 @@
 <script setup>
 // 表单检验（账号名+密码）
 import { ref } from 'vue'
+// import { loginAPI } from '@/apis/user'
+import 'element-plus/theme-chalk/el-message.css'
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
+const userStore = useUserStore()
 // 1. 准备表单对象
 const form = ref({
-    account: '',
-    password: '',
+    account: 'heima282',
+    password: 'hm#qd@23!',
     agree: true
 })
 
@@ -44,15 +50,23 @@ const rules = ref({
 
 // 3. 获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
+    const { account, password } = form.value
     // 调用实例方法
-    formRef.value.validate((valid) => {
+    formRef.value.validate(async(valid) => {
         // valid: 所有表单都通过校验 才为true
         console.log(valid);
         // 以valid 作为判断条件，如果通过校验才执行登录逻辑
         if (valid) {
             // TODO LOGIN
-
+            // const res = await loginAPI({ account, password })
+            // console.log(res);
+            await userStore.getUserInfo({ account, password })
+            // 1. 提示用户
+            ElMessage({type: 'success', message: '登录成功'})
+            // 2. 跳转首页
+            router.replace({ path: '/'})
         }
     })
 }
