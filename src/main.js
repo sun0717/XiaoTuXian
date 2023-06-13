64612d620597aa35fad857d38979eed132df6966
sun <743685264@qq.com>
2023-06-14 00:51:24 +0800

    登录—pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 // 全局指令注册
 import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
@@ -11,8 +12,10 @@ import router from './router'
 
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
