75290d93e71de111c48e05f126db289001d17cba
sun <743685264@qq.com>
2023-08-04 16:33:50 +0800

    退出登录时清除购物车数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -52,6 +52,11 @@ export const useCartStore = defineStore(
       }
     };
 
+    // 清除购物车
+    const clearCart = () => {
+      cartList.value = []
+    }
+
     // 单选功能
     const singleCheck = (skuId, selected) => {
       // 通过skuId找到要修改的那一项，然后把它的selected修改为传过来的selected
@@ -104,6 +109,7 @@ export const useCartStore = defineStore(
       allCheck,
       selectedCount,
       selectedPrice,
+      clearCart
     };
   },
   {
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,20 +2,26 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
+import { useCartStore } from './cartStore'
 export const useUserStore = defineStore('user', () => {
+    const cartStore = useCartStore();
     // 1. 定义管理用户数据的state
     const userInfo = ref({
         
-    })
+    });
+
     // 2. 定义获取接口数据的action函数
     const getUserInfo = async ({ account, password }) => {
-        const res = await loginAPI({ account, password })
-        userInfo.value = res.result
+        const res = await loginAPI({ account, password });
+        userInfo.value = res.result;
     }
     // 退出时清除用户信息
     const clearUserInfo = () => {
-        userInfo.value = {}
+        userInfo.value = {};
+        // 执行清除购物车的action 
+        cartStore.clearCart();
     }
+
     // 3. 以对象的格式把 state 和 action return 
     return {
         userInfo,
